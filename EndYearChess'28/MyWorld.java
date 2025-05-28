d42b54c3974ee0b6df15ee7188f6a7aea9c2d81a
chrrod28ATCS <chrod32110@gmail.com>
2025-05-28 13:23:21 -0400

    ChessBoard image as world background

--- EndYearChess'28/MyWorld.java
+++ EndYearChess'28/MyWorld.java
@@ -16,6 +16,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(640, 640, 1); 
+        GreenfootImage board = new GreenfootImage("ChessBoard.png");
+        board.scale(getWidth(), getHeight());
+        setBackground(board);
     }
 }
